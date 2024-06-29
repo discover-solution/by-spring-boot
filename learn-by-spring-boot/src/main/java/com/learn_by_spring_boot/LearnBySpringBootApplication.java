0ae2cc569bdf3d958e6ddd7eda3516586e834c55
Sonu Singh <grjdeveloper@gmail.com>
2024-06-29 12:10:21 +0530

    Starting Application
    Starting Application

---
+++ learn-by-spring-boot/src/main/java/com/learn_by_spring_boot/LearnBySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.learn_by_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnBySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnBySpringBootApplication.class, args);
+	}
+
+}
---
+++ learn-by-spring-boot/src/test/java/com/learn_by_spring_boot/LearnBySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn_by_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnBySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
